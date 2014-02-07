a8b7c15cf5c4522d91b6a9c291ead64ca7eaa84f
Les Aker <me@lesaker.org>
2014-02-07 15:26:03 -0500

    ugh file handles

--- scripts/debug.rb
+++ scripts/debug.rb
@@ -4,10 +4,10 @@
 return unless File.exists? '/tmp/debug_mode'
 
 # Copy root password for maker to new root
-lines = [
-  open('/etc/shadow').readlines.first,
-  *File.open(@config['paths']['build'] + '/etc/shadow').readlines[1..-1]
-]
+lines = [open('/etc/shadow').readlines.first]
+File.open(@config['paths']['build'] + '/etc/shadow') do |fh|
+  lines << *fh.readlines
+end
 File.open(@config['paths']['build'] + '/etc/shadow', 'w') do |fh|
   lines.each { |line| fh.write line }
 end
