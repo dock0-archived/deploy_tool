c8ee09565e5d2b78e07189f034e7b08adcfde534
Les Aker <me@lesaker.org>
2014-02-07 14:22:01 -0500

    File.open and open are not the same

--- scripts/debug.rb
+++ scripts/debug.rb
@@ -6,8 +6,8 @@ return unless File.exists? '/tmp/debug_mode'
 # Copy root password for maker to new root
 lines = [
   open('/etc/shadow').readlines.first,
-  *open(@config['paths']['build'] + '/etc/shadow').readlines[1..-1]
+  *File.open(@config['paths']['build'] + '/etc/shadow').readlines[1..-1]
 ]
-open(@config['paths']['build'] + '/etc/shadow', 'w') do |fh|
+File.open(@config['paths']['build'] + '/etc/shadow', 'w') do |fh|
   lines.each { |line| fh.write line }
 end
