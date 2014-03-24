0856017f9d3f4fff22006fe16c2827f98a0e6607
Les Aker <me@lesaker.org>
2014-03-24 16:04:01 -0400

    fixed ssh magic

--- scripts/debug.rb
+++ scripts/debug.rb
@@ -12,12 +12,3 @@ system "sed -i 's/ListenAddress/#ListenAddress/' \
 # Allow port 22 through the firewall
 system "sed -i 's/INPUT DROP/INPUT ACCEPT/' \
   #{@config['paths']['build']}/etc/iptables/iptables.rules"
-
-# Copy root password for maker to new root
-lines = [open('/etc/shadow').readlines.first]
-File.open(@config['paths']['build'] + '/etc/shadow') do |fh|
-  fh.readlines[1..-1].each { |line| lines << line }
-end
-File.open(@config['paths']['build'] + '/etc/shadow', 'w') do |fh|
-  lines.each { |line| fh.write line }
-end
