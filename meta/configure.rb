9aaf6d3adc32bacf0eacc6221a744b3e64d7c4d3
Les Aker <me@lesaker.org>
2014-12-16 20:52:52 -0500

    clean up configure script

--- meta/configure.rb
+++ meta/configure.rb
@@ -1,10 +1,20 @@
 #!/usr/bin/env ruby
 
 require 'net/http'
+require 'dock0'
+
+HOSTNAME = ARGV.first || fail('Please supply a hostname')
+CONFIG_FILES = ['config.yaml', "configs/#{HOSTNAME}.yaml"]
+CONFIG = CONFIG_FILES.each_with_object({}) do |file, obj|
+  next unless File.exist? file
+  obj.merge! YAML.load(File.read(file))
+end
+
+puts 'Building config tarball'
+Dock0.easy_mode :Config, *CONFIG_FILES
 
 puts 'Waiting for config flag'
-
-conn = Net::HTTP.new('grego.a-rwx.org', 1002)
+conn = Net::HTTP.new("#{HOSTNAME}#{CONFIG['domain']}", 1002)
 conn.open_timeout = 2
 begin
   sleep 5
@@ -13,6 +23,7 @@ rescue Net::OpenTimeout
   retry
 end
 
+puts 'Sending tarball'
 ssh_options = [
   'StrictHostKeyChecking=no',
   'UserKnownHostsFile=/dev/null',
@@ -21,3 +32,5 @@ ssh_options = [
 ]
 system "scp #{ssh_options.map { |x| '-o' + x }.join(' ')} build.tar.gz akerl@grego.a-rwx.org:/tmp/"
 system "ssh #{ssh_options.map { |x| '-o' + x }.join(' ')} akerl@grego.a-rwx.org touch /tmp/.keep"
+
+puts 'Done!'
