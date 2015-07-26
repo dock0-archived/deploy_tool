f4f1bbe83e8194f95406c3a3f303be4663d54d68
Les Aker <me@lesaker.org>
2015-07-26 00:31:50 -0400

    clean up deploy script w/ lib

--- meta/deploy.rb
+++ meta/deploy.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
 require 'linodeapi'
-require 'json'
 require 'yaml'
 require 'securerandom'
 require 'meld'
+require_relative 'helpers'
 
 HOSTNAME = ARGV.first || fail('Please supply a hostname')
 CONFIG_FILES = ['config.yaml', "configs/#{HOSTNAME}.yaml"]
@@ -14,84 +14,47 @@ CONFIG = CONFIG_FILES.each_with_object({}) do |file, obj|
 end
 API_IDS = CONFIG['api_ids']
 
-def jobs_running?(linodeid)
-  jobs = API.linode.job.list(linodeid: linodeid)
-  jobs.select { |job| job[:host_finish_dt] == '' }.length > 0
-end
+wrapper = APIWrapper.new
 
-def wait_for_jobs(linodeid)
-  while jobs_running? linodeid
-    print '.'
-    sleep 5
-  end
-  puts
-end
-
-api_key = `./meta/getkey.rb`
-fail('API key request failed') if api_key.empty?
-API = LinodeAPI::Raw.new(apikey: api_key)
-
-puts 'Updating StackScript'
-API.stackscript.update(
+auts 'Updating StackScript'
+wrapper.api.stackscript.update(
   stackscriptid: API_IDS['stackscript'],
   distributionidlist: API_IDS['distribution'],
   script: File.read(CONFIG['stackscript'])
 )
 
-linode = API.linode.list.find { |l| l[:label] == HOSTNAME }
-LINODE_ID = linode.linodeid || fail('Linode not found')
-
-existing = {
-  configs: API.linode.config.list(linodeid: LINODE_ID),
-  disks: API.linode.disk.list(linodeid: LINODE_ID)
-}
-
-puts 'Shutting down and removing existing data'
-API.linode.shutdown(linodeid: LINODE_ID)
-existing[:configs].each do |config|
-  API.linode.config.delete(linodeid: LINODE_ID, configid: config[:configid])
-end
-existing[:disks].each do |disk|
-  API.linode.disk.delete(linodeid: LINODE_ID, diskid: disk[:diskid])
-end
-
-wait_for_jobs LINODE_ID
+wrapper.delete_all!
+wrapper.wait_for_jobs
 
 puts 'Creating new disks'
 DISKS = {}
 CONFIG['disks'].each do |disk|
   disk = Hash[disk.map { |k, v| [k.to_sym, v] }]
-  result = API.linode.disk.create disk.merge(linodeid: LINODE_ID)
-  DISKS[disk[:label].to_sym] = result[:diskid]
+  DISKS[disk[:label].to_sym] = wrapper.create_disk disk
 end
 
 ROOT_PW = SecureRandom.hex(24)
 
-DISKS[:maker] = API.linode.disk.createfromstackscript(
-  linodeid: LINODE_ID,
+DISKS[:maker] = wrapper.create_from_stackscript(
   stackscriptid: API_IDS['stackscript'],
   distributionid: API_IDS['distribution'],
   rootpass: ROOT_PW,
   label: 'maker',
   size: 1024,
-  stackscriptudfresponses: { name: HOSTNAME }.to_json
+  stackscriptudfresponses: '{}'
 )[:diskid]
 DISKS[:finnix] = API_IDS['finnix']
 
-CONFIG_ID = API.linode.config.create(
+CONFIG_ID = wrapper.create_config(
   kernelid: API_IDS['stock_kernel'],
   disklist: DISKS.values_at(:maker, :finnix, :root, :lvm).join(','),
   label: 'dock0',
-  linodeid: LINODE_ID
-)[:configid]
+)
 
 puts 'Booting maker image'
-API.linode.boot(linodeid: LINODE_ID, configid: CONFIG_ID)
-sleep 2
-wait_for_jobs LINODE_ID
+wrapper.boot(CONFIG_ID)
 
-API.linode.config.update(
-  linodeid: LINODE_ID,
+wrapper.config_update(
   configid: CONFIG_ID,
   helper_depmod: false,
   helper_distro: false,
---
+++ meta/helpers.rb
@@ -0,0 +1,85 @@
+module APIWrapper
+  class << self
+    def new(*args)
+      self::Wrapper.new(*args)
+    end
+  end
+
+  class Wrapper
+    def initialize(hostname)
+      @hostname = hostname
+    end
+
+    def jobs_running?
+      jobs = api.linode.job.list(linodeid: linodeid)
+      jobs.select { |job| job[:host_finish_dt] == '' }.length > 0
+    end
+
+    def wait_for_jobs
+      while jobs_running? linodeid
+        print '.'
+        sleep 5
+      end
+      puts
+    end
+
+    def delete_all!
+      puts 'Shutting down and removing existing data'
+      api.linode.shutdown(linodeid: linodeid)
+      configs, disks = existing
+      configs.each do |c|
+        api.linode.config.delete(linodeid: linodeid, configid: c[:configid])
+      end
+      disks.each do |d|
+        api.linode.disk.delete(linodeid: linodeid, diskid: d[:diskid])
+      end
+    end
+
+    def create_disk(params)
+      res = api.linode.disk.create params.merge(linodeid: linodeid)
+      res[:diskid]
+    end
+
+    def create_from_stackscript(params)
+      res = api.linode.disk.createfromstackscript(
+        params.merge(linodeid: linodeid)
+      )
+      res[:diskid]
+    end
+
+    def create_config(params)
+      res = api.linode.config.create params.merge(linodeid: linodeid)
+      res[:configid]
+    end
+
+    def update_config(params)
+      res = api.linode.config.update params.merge(linodeid: linodeid)
+    end
+
+    def boot(configid)
+      api.linode.boot(linodeid: linodeid, configid: configid)
+      sleep 2
+      wait_for_jobs
+    end
+
+    def api
+      return @api if @api
+      api_key = `./meta/getkey.rb`
+      fail('API key request failed') if api_key.empty?
+      @api = LinodeAPI::Raw.new(apikey: api_key)
+    end
+
+    def linodeid
+      return @linodeid if @linodeid
+      linode = api.linode.list.find { |l| l[:label] == @hostname }
+      @linodeid = linode.linodeid || fail('Linode not found')
+    end
+
+    def existing
+      [
+        api.linode.config.list(linodeid: linodeid),
+        api.linode.disk.list(linodeid: linodeid)
+      ]
+    end
+  end
+end
