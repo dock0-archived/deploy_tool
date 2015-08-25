d036a940f4e216d548914ba9c649f7a193e60e5a
Les Aker <me@lesaker.org>
2015-08-25 11:54:09 -0400

    update getkey to handle initial loading, fixes #29

--- meta/lib/getkey.rb
+++ meta/lib/getkey.rb
@@ -1,10 +1,41 @@
 #!/usr/bin/env ruby
 
 require 'keychain'
+require 'highline/import'
+require 'linodeapi'
 
-keychain = ENV['DOCK0_KEYCHAIN'] || Keychain.default.path
+keychain_path = ENV['DOCK0_KEYCHAIN'] || Keychain.default.path
+KEYCHAIN = Keychain.open keychain_path
+SERVICE = 'linode-api'
 
-api_key = Keychain.open keychain
-api_key = api_key.generic_passwords.where(service: 'linode-api')
-fail('Failed to load keychain') unless api_key.first
-puts api_key.first.password
+def load_from_keychain
+  entry = KEYCHAIN.generic_passwords.where(service: SERVICE).first
+  fail(KeyError, 'Keychain item not found') unless entry
+  entry.password
+end
+
+def save_to_keychain(key)
+  KEYCHAIN.generic_passwords.create(service: SERVICE, password: key)
+end
+
+def load_from_prompt
+  prompt = HighLine.new(STDIN, STDERR)
+  username = prompt.ask('Linode Manager Username: ')
+  password = prompt.ask('Linode Manager Password: ') { |q| q.echo = '*' }
+  twofactor = prompt.ask('Linode Manager 2FA token: ')
+  api = LinodeAPI::Raw.new(
+    username: username,
+    password: password,
+    token: twofactor)
+  api.apikey
+end
+
+begin
+  key = load_from_keychain
+rescue KeyError
+  key = load_from_prompt
+  save_to_keychain(key)
+end
+
+fail('Failed to load credentials') unless key
+puts key
