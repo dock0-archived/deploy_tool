5697360fa9e376b200354b9ebdd472287f2014a6
Les Aker <me@lesaker.org>
2016-01-15 16:13:03 -0500

    add scripts for docker container startup

---
+++ scripts/docker.rb
@@ -0,0 +1,21 @@
+##
+# Add template files for spawning Docker containers
+
+require 'fileutils'
+require 'erb'
+
+return
+return unless @config[:containers]
+
+TEMPLATE_DIR = @paths[:assets] || './assets'
+TEMPLATE_FILE = File.join(TEMPLATE_DIR, 'container_init.erb')
+TEMPLATE = ERB.new(File.read(TEMPLATE_FILE), nil, '<>')
+
+@config[:containers].each do |container|
+  service = container[:name].gsub('/', '_')
+  dir = "#{@paths[:build]}/etc/s6/services/#{service}"
+  script = "#{dir}/run"
+
+  FileUtils.mkdir_p dir
+  File.open(script, 'w') { |fh| fh.write TEMPLATE.result(binding) }
+end
