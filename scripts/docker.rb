a6047003a61faaab45153e6e9c61b8c81d133124
Les Aker <me@lesaker.org>
2016-01-16 17:37:57 -0500

    add envfile support

--- scripts/docker.rb
+++ scripts/docker.rb
@@ -11,15 +11,41 @@ def load_template(name)
   ERB.new(File.read(file), nil, '<>')
 end
 
-TEMPLATE_DIR = @paths[:assets] || './assets'
+def load_envfile(container, suffix, &block)
+  file = File.join ENVFILE_DIR, "#{container[:envfile]}.#{suffix}"
+  return unless File.exist? file
+  block.call(file)
+end
+
+def add_envfile(container)
+  return unless container[:envfile]
+  data = load_envfile(container, 'txt') { |x| File.read(x) }
+  data ||= load_envfile(container, 'rb') { |x| `#{x}` }
+  fail("No envfile found: #{container[:envfile]}") unless data
+  target_file = File.join ENVFILE_TARGET_DIR, container[:service]
+  FileUtils.mkdir_p ENVFILE_TARGET_DIR
+
+  File.open(target_file, 'w', 0600) { |fh| fh << data }
+end
+
+BASE_DIR = @paths[:docker] || './docker'
+
+TEMPLATE_DIR = File.join(BASE_DIR, 'templates')
 TEMPLATES = Hash[%w(run finish).map { |x| [x, load_template(x)] }]
 
+ENVFILE_DIR = File.join(BASE_DIR, 'envfiles')
+
+TARGET_DIR = File.join(@paths[:build], 'templates')
+SERVICE_TARGET_DIR = File.join(TARGET_DIR, 'etc', 's6', 'service')
+ENVFILE_TARGET_DIR = File.join(TARGET_DIR, 'etc', 'docker')
+
 @config[:containers].each do |container|
-  service = container[:name].gsub('/', '_')
-  dir = "#{@paths[:build]}/templates/etc/s6/service/#{service}"
+  container[:service] = container[:name].gsub('/', '_')
+  dir = File.join SERVICE_TARGET_DIR, "container_#{container[:service]}"
   TEMPLATES.each do |file, template|
     script = "#{dir}/#{file}"
     FileUtils.mkdir_p dir
     File.open(script, 'w', 0755) { |fh| fh.write template.result(binding) }
   end
+  add_envfile(container)
 end
